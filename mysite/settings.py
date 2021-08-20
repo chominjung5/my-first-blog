b518b691f094f854f90932df48c39baaceb23cbd
mjcho <mjcho@okpos.co.kr>
2021-08-20 13:44:58 +0900

    Added settings files

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'djangogirls',
+                'USER': 'mjcho',
+                'PASSWORD': '1234!',
+                'HOST': 'localhost',
+                'PORT': '5432',
     }
+
+
 }
+
+
 
 
 # Password validation
@@ -113,7 +123,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
